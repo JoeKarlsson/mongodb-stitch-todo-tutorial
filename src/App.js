18020687e7fe6baa9ab17f24c1fde0fccd7cafd3
dracero <diego.racero@gmail.com>
2020-10-14 16:11:29 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -14,29 +14,57 @@ class App extends Component {
       value: ""
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.displayTodos = this.displayTodos.bind(this);
-    this.addTodo = this.addTodo.bind(this);
   }
 
   componentDidMount() {
     // Initialize the App Client
-    this.client = Stitch.initializeDefaultAppClient("todotutorial-pchnc");
+    this.client = Stitch.initializeDefaultAppClient("waeinv-synha");
     // Get a MongoDB Service Client, used for logging in and communicating with Stitch
     const mongodb = this.client.getServiceClient(
       RemoteMongoClient.factory,
       "mongodb-atlas"
     );
     // Get a reference to the todo database
-    this.db = mongodb.db("todo");
-
+    this.db = mongodb.db("coreTec");
     this.displayTodosOnLoad();
   }
 
-  displayTodos() {
+  /*displayTodos() {
     this.db
-      .collection("item")
-      .find({}, { limit: 1000 })
+      .collection("inventory")
+      .find({}, { limit: 1 })
+      .asArray()
+      .then(todos => {
+        this.setState({
+          todos
+        });
+        var dosc = todos.map(ele => ele.LineCard)
+        console.log(this.state.todos) 
+      });
+  }*/
+
+  displayTodos() {
+    const agg = [
+      {
+        $project: {
+          in: "$TrafficIn(Maximum)",
+          out: "$TrafficOut(Maximum)",
+          name: "$name",
+          day: "$day",
+          month: "$month",
+          year: "$year"
+        }
+      },
+      {
+        $sort: {
+          month: -1
+        }
+      }
+    ];
+    this.db
+      .collection("coreInOut")
+      .aggregate(agg)
       .asArray()
       .then(todos => {
         this.setState({
@@ -52,44 +80,43 @@ class App extends Component {
       .catch(console.error);
   }
 
-  addTodo(event) {
-    event.preventDefault();
-    const { value } = this.state;
-
-    this.db
-      .collection("item")
-      .insertOne({
-        owner_id: this.client.auth.user.id,
-        item: value
-      })
-      .then(this.displayTodos);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
   render() {
     return (
       <div className="App">
-        <h3>This is a todo app</h3>
+        <h3>Trafico</h3>
         <hr />
-        <p>Add a Todo Item:</p>
-        <form onSubmit={this.addTodo}>
-          <label>
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          {this.state.todos.map(todo => {
-            return <li>{todo.item}</li>;
-          })}
-        </ul>
+        <table>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.day}</tr>;
+            })}
+          </td>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.month}</tr>;
+            })}
+          </td>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.year}</tr>;
+            })}
+          </td>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.name}</tr>;
+            })}
+          </td>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.in / 1000}</tr>;
+            })}
+          </td>
+          <td>
+            {this.state.todos.map(todo => {
+              return <tr>{todo.out / 1000}</tr>;
+            })}
+          </td>
+        </table>
       </div>
     );
   }
